4c3a8cfeea9545aedf95cc6f205b9a0b76787cd1
John Raugas <didascolia@gmail.com>
2026-06-27 23:53:20 +0400

    feat: Add background color changing functionality

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Set up the color selector widget
     Coloris({
         alpha: false,
         theme: 'polaroid',
+    })
+    // Change page color when a new color is selected
+    document.getElementById('bg-color').addEventListener('change', () => {
+        let newColor = document.getElementById('bg-color').value;
+        document.getElementById('current-page-svg')
+            .style.backgroundColor = newColor;
     })
 })
