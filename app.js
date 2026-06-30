b48d1dc10a3328fb30d6d4d2cfacaab7447e011d
John Raugas <didascolia@gmail.com>
2026-07-01 00:36:19 +0400

    feat: Add letters to the puzzle

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ let documentWidth = 8.27;
 let documentHeight = 11.69;
 let marginInside = 0.75;
 let marginOutside = 0.5;
-let paddingLeft = 0.6;
-let paddingRight = 0.6;
-let paddingTop = 0.8;
-let paddingBottom = 1.2;
+let paddingLeft = 0.5;
+let paddingRight = 0.5;
+let paddingTop = 0.5;
+let paddingBottom = 0.5;
 // These values are dimensionless ratios of various elements to the circle size
 let gapVertical = 0.60; // gap between rows of circles
 let gapHorizontal = 0.15; // gap between columns of circles
 let gapPicture = 0.30; // gap between the picture and the circles
 let pictureScale = 0.80;
+let letterScale = 0.80;
 let strokeThickness = 0.035;
 // These values are countable numbers
 let wordLength = 3;
-let wordNumber = 4;
 // These values are boolean
 let insideIsLeft = true;
 
@@ -35,12 +35,26 @@ if (insideIsLeft) {
     marginRight = marginInside;
 }
 
-/* Transform a number into a string that ends with 'px' */
+// ------- PUZZLE CONTENT -------
+let wordList = [
+    { word: ' at', emoji: '&#128049;' },
+    { word: ' an', emoji: '&#128656;' },
+    { word: ' at', emoji: '&#128000;' },
+    { word: ' at', emoji: '&#129415;' },
+];
+let wordNumber = wordList.length;
+
+/* Transform a number into a string that ends with 'px'. */
 function appendPx(n) {
     return `${n}px`
 }
 
-/* Update the height and width of the SVG with the global variable values */
+/* Create an SVG element by its name */
+function createSVGElement(name) {
+    return document.createElementNS('http://www.w3.org/2000/svg', name);
+}
+
+/* Update the height and width of the SVG with the global variable values. */
 function updateDocumentSize() {
     const pageDiv = document.getElementById('current-page');
     const pageSVG = document.getElementById('current-page-svg');
@@ -52,31 +66,81 @@ function updateDocumentSize() {
     pageSVG.setAttribute('height', documentHeight * scale);
 }
 
-/* Update the page margins in the SVG with the global variable values */
+/* Update the page margins in the SVG with the global variable values. */
 function updatePageMargins() {
-    const topLine = document.getElementById('top-margin-line');
-    const leftLine = document.getElementById('left-margin-line');
-    const rightLine = document.getElementById('right-margin-line');
-    const bottomLine = document.getElementById('bottom-margin-line');
-    topLine.setAttribute("y1", marginTop * scale)
-    topLine.setAttribute("y2", marginTop * scale)
-    topLine.setAttribute("x1", 0)
-    topLine.setAttribute("x2", documentWidth * scale)
-    leftLine.setAttribute("x1", marginLeft * scale)
-    leftLine.setAttribute("x2", marginLeft * scale)
-    leftLine.setAttribute("y1", 0)
-    leftLine.setAttribute("y2", documentHeight * scale)
-    rightLine.setAttribute("x1", (documentWidth - marginRight) * scale)
-    rightLine.setAttribute("x2", (documentWidth - marginRight) * scale)
-    rightLine.setAttribute("y1", 0)
-    rightLine.setAttribute("y2", documentHeight * scale)
-    bottomLine.setAttribute("y1", (documentHeight - marginBottom) * scale)
-    bottomLine.setAttribute("y2", (documentHeight - marginBottom) * scale)
-    bottomLine.setAttribute("x1", 0)
-    bottomLine.setAttribute("x2", documentWidth * scale)
+    const lines = [[
+        document.getElementById('top-margin-line'),
+        document.getElementById('left-margin-line'),
+        document.getElementById('right-margin-line'),
+        document.getElementById('bottom-margin-line'),
+    ], [
+        document.getElementById('top-margin-hl-line'),
+        document.getElementById('left-margin-hl-line'),
+        document.getElementById('right-margin-hl-line'),
+        document.getElementById('bottom-margin-hl-line'),
+    ]];
+    for (const [topLine, leftLine, rightLine, bottomLine] of lines) {
+        topLine.setAttribute('y1', marginTop * scale)
+        topLine.setAttribute('y2', marginTop * scale)
+        topLine.setAttribute('x1', 0)
+        topLine.setAttribute('x2', documentWidth * scale)
+        leftLine.setAttribute('x1', marginLeft * scale)
+        leftLine.setAttribute('x2', marginLeft * scale)
+        leftLine.setAttribute('y1', 0)
+        leftLine.setAttribute('y2', documentHeight * scale)
+        rightLine.setAttribute('x1', (documentWidth - marginRight) * scale)
+        rightLine.setAttribute('x2', (documentWidth - marginRight) * scale)
+        rightLine.setAttribute('y1', 0)
+        rightLine.setAttribute('y2', documentHeight * scale)
+        bottomLine.setAttribute('y1', (documentHeight - marginBottom) * scale)
+        bottomLine.setAttribute('y2', (documentHeight - marginBottom) * scale)
+        bottomLine.setAttribute('x1', 0)
+        bottomLine.setAttribute('x2', documentWidth * scale)
+    }
 }
 
-/* Update the puzzle's geometry with the global variable values */
+/* Update the SVG elements of which the puzzle is composed.
+ *
+ * Note that this erases all geometry set on the puzzle, as all elements
+ * are deleted and then created again. */
+function updatePuzzleContents() {
+
+    // First, erase all previous groups and objects
+    document.getElementById('current-page-content').remove()
+    const pageContent = createSVGElement('g');
+    pageContent.id = 'current-page-content';
+    document.getElementById('current-page-svg').appendChild(pageContent);
+
+    for (let i = 0; i < wordNumber; i++) {
+        // Create a group for each row
+        const row = createSVGElement('g');
+        row.id = `row${i}-group`;
+        pageContent.appendChild(row);
+        // Add a picture
+        const picture = createSVGElement('text');
+        picture.classList.add('picture', `row${i}`);
+        picture.innerHTML = wordList[i]['emoji'];
+        row.appendChild(picture);
+
+        for (let j = 0; j < wordLength; j++) {
+            // Make a circle for each letter of the word
+            const circle = createSVGElement('circle');
+            circle.setAttribute('stroke', 'black');
+            circle.setAttribute('fill', 'white');
+            circle.classList.add('circle', `row${i}`, `col${j}`);
+            row.appendChild(circle)
+            // Add pre-filled letters
+            const letter = createSVGElement('text');
+            letter.setAttribute('text-anchor', 'middle');
+            letter.setAttribute('dominant-baseline', 'central');
+            letter.classList.add('letter', `row${i}`, `col${j}`);
+            letter.innerHTML = wordList[i]['word'][j].toUpperCase();
+            row.appendChild(letter);
+        }
+    }
+}
+
+/* Update the puzzle's geometry with the global variable values. */
 function updatePuzzleGeometry() {
     const availableWidth = documentWidth
         - marginLeft
@@ -99,7 +163,7 @@ function updatePuzzleGeometry() {
 
     // Update each picture and circle
     for (let i = 0; i < wordNumber; i++) {
-        const picture = document.getElementById(`picture-row${i}`);
+        const picture = document.querySelector(`.picture.row${i}`);
         picture.setAttribute('x', (marginLeft + paddingLeft) * scale);
         picture.setAttribute('y', scale * (
             marginTop
@@ -109,8 +173,7 @@ function updatePuzzleGeometry() {
         ));
         picture.style.fontSize = appendPx(circleSize * pictureScale * scale);
         for (let j = 0; j < wordLength; j++) {
-            const circle = document.querySelector(
-                `.circle-row${i}.circle-col${j}`);
+            const circle = document.querySelector(`.circle.row${i}.col${j}`);
             circle.setAttribute('cx', scale * (
                 marginLeft
                 + paddingLeft
@@ -128,6 +191,22 @@ function updatePuzzleGeometry() {
             circle.setAttribute('r', scale * circleRadius);
             circle.setAttribute('stroke-width',
                 circleSize * strokeThickness * scale);
+            const letter = document.querySelector(`.letter.row${i}.col${j}`);
+            letter.setAttribute('x', scale * (
+                marginLeft
+                + paddingLeft
+                + circleSize
+                + gapPicture * circleSize
+                + j * (1+gapHorizontal) * circleSize
+                + circleRadius
+            ));
+            letter.setAttribute('y', scale * (
+                marginTop
+                + paddingTop
+                + i * (1+gapVertical) * circleSize
+                + circleRadius
+            ));
+            letter.style.fontSize = appendPx(letterScale * circleSize * scale);
         }
     }
 }
@@ -147,10 +226,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // Update page dimensions
     const selectedValue = document.getElementById('document-size').value.split('x');
     const [selectedWidth, selectedHeight] = selectedValue;
-    documentWidth = parseInt(selectedWidth);
-    documentHeight = parseInt(selectedHeight);
+    documentWidth = parseFloat(selectedWidth);
+    documentHeight = parseFloat(selectedHeight);
     updateDocumentSize();
     updatePageMargins();
+    updatePuzzleContents();
     updatePuzzleGeometry();
 
     // --- PUZZLE DYNAMIC CONTROLS --
